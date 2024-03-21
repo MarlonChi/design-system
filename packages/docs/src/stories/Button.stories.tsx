9f142b0c7459560705ce650fbf058711f90b8692
MarlonChi <marlonchiodelli@hotmail.com>
2024-03-20 21:15:18 -0300

    docs: tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@MarlonChi-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Enviar',
